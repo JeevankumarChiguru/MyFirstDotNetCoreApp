ee23855b88498f91085a6f71586c38ea205d06ec
Jeevankumar <Jeevankumar@JEEVANKUMAR>
2023-02-07 00:04:45 +0530

    added mvc to application

--- BooksStore/BooksStore/Startup.cs
+++ BooksStore/BooksStore/Startup.cs
@@ -16,6 +16,7 @@ namespace BooksStore
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -25,15 +26,19 @@ namespace BooksStore
             {
                 app.UseDeveloperExceptionPage();
             }
+            //app.Use(async(context, next)=>
+            //{
+            //    await context.Response.WriteAsync("hellow from my first middleware");
 
+            //    await next();
+            //    await context.Response.WriteAsync("hellow from my first middleware response");
+            //});
+            
             app.UseRouting();
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapDefaultControllerRoute();
             });
         }
     }
